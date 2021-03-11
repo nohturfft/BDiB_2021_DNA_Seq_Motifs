058874ac7e4fb88691d1911d05eaadcb0386bde1
nohturfft <nohturfft@gmail.com>
2021-03-11 20:11:49 +0000

    Pre-loading secondary packages

--- packages.R
+++ packages.R
@@ -2,43 +2,35 @@
 # Load packages #
 #################
 
-# Help for specific packages can be obtained as shown in the code chunk below
-# (remove the '#' to run the code).  
-# help(package="ChIPpeakAnno")
-# help(package="biomaRt")
-
-# requireNamespace("readr")
-# requireNamespace("knitr")
-# library(scales)
-
-# **LChIPpeakAnno package (https://doi.org/doi:10.18129/B9.bioc.ChIPpeakAnno)  
-# Annotation of peaks identified by ChIP-seq or other experiments that produce
-# large numbers of chromosome coordinates. 
-# library(ChIPpeakAnno)
-
-# biomaRt package (https://doi.org/doi:10.18129/B9.bioc.biomaRt)  
-# "Interface to BioMart databases (e.g. Ensembl, COSMIC, Wormbase and Gramene)".
-# Used to map gene identifiers.
-# library(biomaRt)
-
-# org.Hs.eg.db package (https://doi.org/doi:10.18129/B9.bioc.org.Hs.eg.db)  
-# A package with human genome annotation data.
-# library(org.Hs.eg.db)
-
-# rstudioapi package (https://cran.r-project.org/package=rstudioapi)  
-# The 'rstudioapi' package is useful for programmatically obtaining the current
-# script name and its parent directory.
-# requireNamespace("rstudioapi")
-
-# pryr package (https://cran.r-project.org/package=pryr)  
-# We will use a function from the 'pryr' package to determine the
-# size of a data object.
-# requireNamespace("pryr")
-
-# magrittr package (https://cran.r-project.org/package=magrittr)  
-# "Provides a mechanism for chaining commands with a new forward-pipe operator, %>%.
-# This operator will forward a value, or the result of an expression, into the next
-# function call/expression."  
 library(magrittr)
+library(dplyr)
+library(readr)
+requireNamespace("pryr")
+
+library(parallel) # required for BiocGenerics
+library(BiocGenerics) # required for BSgenome
+library(stats4) # required for S4Vectors
+library(S4Vectors) # required for BSgenome
+library(IRanges) # required for BSgenome
+library(GenomeInfoDb) # required for BSgenome
+library(GenomicRanges) # required for BSgenome
+library(XVector) # required for Biostrings
+library(Biostrings) # required for BSgenome
+
+library(rtracklayer) # required for BSgenome
+library(BSgenome) # required for rGADEM
+library(grid) # required for seqLogo
+library(seqLogo) # required for rGADEM
+library(rGADEM)
+
+library(futile.logger) # required for VennDiagram
+library(VennDiagram) # required for ChIPpeakAnno
+library(ChIPpeakAnno)
+
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(JASPAR2014)
+library(MotIV)
+
+
 
 
